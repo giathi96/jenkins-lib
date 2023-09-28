404e8ff710d6327bf9495fd3c374d2e162e6c48d
giathirahlan <giathirahlan@gmail.com>
2023-09-28 23:42:00 +0700

    TESTING

--- vars/main_pipeline_1.groovy
+++ vars/main_pipeline_1.groovy
@@ -34,9 +34,15 @@ def call(body){
                                 def command = """
                                     echo "sdadas sdsa<software-rev>Hello</software-rev> sdad sda 192.168.0.1 dsfd 192.168.0.98" | perl -nle 'if (/192\\.168\\.0\\.(\\d{1,3})/ && \$1 >= 0 && \$1 <= 254) { print "192.168.0.\$1"; last; }' 
                                 """
-                                def result = sh(script: command, returnStdout: true).trim()
 
-                                println("RESULT: " + result)
+                                def command2 = """
+                                    echo  | perl -0777 -ne 'if (/<software-rev>(.*?)<\\/software-rev>/s) { print "\$1\\n"; exit; }'
+                                """
+                                def result1 = sh(script: command, returnStdout: true).trim()
+                                def result2 = sh(script: command2, returnStdout: true).trim()
+
+                                println("RESULT1: " + result1)
+                                println("RESULT2: " + result2)
                             }
                         }
                     }
