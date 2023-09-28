5a5c44adfde64008e9390e0a4948ff24b07f1602
giathirahlan <giathirahlan@gmail.com>
2023-09-28 21:52:39 +0700

    TESTING

--- vars/test_matcher.groovy
+++ vars/test_matcher.groovy
@@ -1,5 +1,18 @@
 def call() {
     script {
-        println("Test matcher")
+        def input = "AhelloA"
+        def pattern = /A(.*?)A/
+        def matcher = (input =~ pattern)
+
+        if (matcher.find()) {
+            def result = matcher.group(1)
+            return result
+            println(result)
+        } else {
+            println("No match found")
+            return "KHONG CO"
+        }
+
+        return "XONG"
     }
 }
