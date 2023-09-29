b26df0787685beedb5ea957e92a17989e237fcf2
giathirahlan <giathirahlan@gmail.com>
2023-09-29 11:12:11 +0700

    TESTING

--- vars/main_pipeline_1.groovy
+++ vars/main_pipeline_1.groovy
@@ -31,21 +31,8 @@ def call(body){
                     stage("Parallel stage 2") {
                         steps {
                             script {
-                                def command = """
-                                    echo "sdadas sdsa<software-rev>Hello</software-rev> sdad sda 192.168.0.1 dsfd 192.168.0.98" | perl -nle 'if (/192\\.168\\.0\\.(\\d{1,3})/ && \$1 >= 0 && \$1 <= 254) { print "192.168.0.\$1"; last; }' 
-                                """
-
-                                def command2 = """
-                                    echo "sdadas sdsa<softsware-rev>Hello</softwsare-rev> sdad sda 192.168.0.1 dsfd 192.168.0.98" | perl -0777 -ne 'if (/<software-rev>(.*?)<\\/software-rev>/s) { print "\$1\\n"; exit; }'
-                                """
-                                def result1 = sh(script: command, returnStdout: true).trim()
-                                def result2 = sh(script: command2, returnStdout: true).trim()
-
-                                println("TYPE ${result1.getClass().getName()}")
-                                println("TYPE ${result2.getClass().getName()}")
-                                println("TYPE ${sh(script: command, returnStdout: true).getClass().getName()}")
-                                println("RESULT1: " + result1)
-                                println("RESULT2: " + result2)
+                                println(currentBuild.number)
+                                println(currentBuild.number.getClass().getName())
                             }
                         }
                     }
